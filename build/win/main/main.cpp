1562acc2b45c41910c3ea5d1f94943080b3cdc84
U-SUIRUI\cl.zhan <cl.zhan@sr-zhancl06.suirui.com>
2016-06-20 11:19:04 +0800

    提交windows下的测试版本
    Signed-off-by: U-SUIRUI\cl.zhan <cl.zhan@sr-zhancl06.suirui.com>

---
+++ build/win/main/main.cpp
@@ -0,0 +1,43 @@
+
+#include <string>
+#include <iostream>
+using namespace std;
+
+#include <sstream>
+#include <cstring>
+
+#include "httpjson.h"
+
+//去除首位空格
+string& trim(string &s)
+{
+	if (s.empty())
+	{
+		return s;
+	}
+
+	s.erase(0, s.find_first_not_of(" "));
+	s.erase(s.find_last_not_of(" ") + 1);
+	return s;
+}
+
+int main(int argc, char* argv[])
+{
+
+	std:string url = "http://103.249.255.237/utils/v1/utils/getkey3";
+
+	SRCurrentDllSoType dlltype;
+
+	dlltype.version3 = "2.6";
+	dlltype.id3 = "782bcb4769a8";
+	dlltype.appId = "12345";
+	dlltype.clientModel = "android";
+	dlltype.clientVersion = "1.1";
+	string key3;
+	int ret = HttpGet(url, dlltype, key3);
+
+	printf("ret = %d\n", ret);
+	printf("key3 = %s\n", key3.c_str());
+
+	return 0;
+}
