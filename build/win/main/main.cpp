8ad356b7c2e1ea2c7f00511b04dcd0dc080aef67
U-SUIRUI\cl.zhan <cl.zhan@sr-zhancl06.suirui.com>
2016-06-23 20:20:52 +0800

    interface change
    Signed-off-by: U-SUIRUI\cl.zhan <cl.zhan@sr-zhancl06.suirui.com>

--- build/win/main/main.cpp
+++ build/win/main/main.cpp
@@ -20,17 +20,92 @@ string& trim(string &s)
 	s.erase(s.find_last_not_of(" ") + 1);
 	return s;
 }
+// MacAddress.cpp : Defines the entry point for the console application.
+//
+
+#include <windows.h>
+#include <wincon.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <Nb30.h>
+#pragma comment(lib,"netapi32.lib")  
+
+int GetMac(char * mac)
+{
+	NCB ncb;
+	typedef struct _ASTAT_
+	{
+		ADAPTER_STATUS   adapt;
+		NAME_BUFFER   NameBuff[30];
+	}ASTAT, *PASTAT;
+
+	ASTAT Adapter;
+
+	typedef struct _LANA_ENUM
+	{
+		UCHAR   length;
+		UCHAR   lana[MAX_LANA];
+	}LANA_ENUM;
+
+	LANA_ENUM lana_enum;
+	UCHAR uRetCode;
+	memset(&ncb, 0, sizeof(ncb));
+	memset(&lana_enum, 0, sizeof(lana_enum));
+	ncb.ncb_command = NCBENUM;
+	ncb.ncb_buffer = (unsigned char *)&lana_enum;
+	ncb.ncb_length = sizeof(LANA_ENUM);
+	uRetCode = Netbios(&ncb);
+
+	if (uRetCode != NRC_GOODRET)
+		return uRetCode;
+
+	for (int lana = 0; lana<lana_enum.length; lana++)
+	{
+		ncb.ncb_command = NCBRESET;
+		ncb.ncb_lana_num = lana_enum.lana[lana];
+		uRetCode = Netbios(&ncb);
+		if (uRetCode == NRC_GOODRET)
+			break;
+	}
+
+	if (uRetCode != NRC_GOODRET)
+		return uRetCode;
+
+	memset(&ncb, 0, sizeof(ncb));
+	ncb.ncb_command = NCBASTAT;
+	ncb.ncb_lana_num = lana_enum.lana[0];
+	strcpy((char*)ncb.ncb_callname, "*");
+	ncb.ncb_buffer = (unsigned char *)&Adapter;
+	ncb.ncb_length = sizeof(Adapter);
+	uRetCode = Netbios(&ncb);
+
+	if (uRetCode != NRC_GOODRET)
+		return uRetCode;
+
+	sprintf(mac, "%02X-%02X-%02X-%02X-%02X-%02X",
+		Adapter.adapt.adapter_address[0],
+		Adapter.adapt.adapter_address[1],
+		Adapter.adapt.adapter_address[2],
+		Adapter.adapt.adapter_address[3],
+		Adapter.adapt.adapter_address[4],
+		Adapter.adapt.adapter_address[5]);
+
+	return 0;
+}
+
+
+
 
 int main(int argc, char* argv[])
 {
-
 
 	std:string url = "http://103.249.255.237/utils/v1/utils/getkey3";
 
 	SRCurrentDllSoType dlltype;
 
-	dlltype.version3 = "2.6";
-	dlltype.id3 = "88708c18e451";
+	dlltype.version3 = "26";
+	dlltype.id3 = "408D5C84E4D7";
 	//dlltype.id3 = "78F5FD5CE9E3";
 	dlltype.appId = "12345";
 	dlltype.clientModel = "android";
@@ -40,6 +115,10 @@ int main(int argc, char* argv[])
 
 	printf("ret = %d\n", ret);
 	printf("key3 = %s\n", key3.c_str());
+
+	char   mac[200];
+	GetMac(mac);
+	printf("The Mac Address is : %s   \n", mac);
 
 	return 0;
 }
