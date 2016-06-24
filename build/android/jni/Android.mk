c68f616d41a00cd186ab68893bad5140f0a570e8
cl.zhan <cl.zhan@suirui.com>
2016-06-24 19:17:39 +0800

    android
    Signed-off-by: cl.zhan <cl.zhan@suirui.com>

--- build/android/jni/Android.mk
+++ build/android/jni/Android.mk
@@ -1,29 +1,39 @@
 
 LOCAL_PATH:= $(call my-dir)/../../../
-#include $(CLEAR_VARS)
-#include $(libjingle-root-dir)/../../../thirdparty/lib/android/Android.mk
+
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE          := libjsoncpp
+LOCAL_SRC_FILES := ./thirdparty/lib/android/libjsoncpp.a
+include $(PREBUILT_STATIC_LIBRARY)
 
 
 
 include $(CLEAR_VARS)
 
-
 LOCAL_ARM_MODE := arm 
 LOCAL_PRELINK_MODULE := false
-LOCAL_MODULE := app
+LOCAL_MODULE := libhttp-json
 
 LOCAL_CPPFLAGS := -fexceptions
 
 LOCAL_MODULE_TAGS := optional
 
 #-DJSON_IS_AMALGAMATION 
+
+
+
+#LOCAL_STATIC_LIBRARIES := \
+              gnustl_static \
 	
 	
 LOCAL_SRC_FILES := \
 				src/httpClient.cpp \
 				src/httpjson.cpp \
 				src/JsonBase.cpp \
-				src/main.cpp \
+			
+			#	src/main.cpp \
 
 
 #jsoncpp.cpp
@@ -36,10 +46,15 @@ LOCAL_C_INCLUDES :=  \
 
 #LOCAL_STATIC_LIBRARIES := jsoncpp curl 
 
-LOCAL_LDLIBS += $(LOCAL_PATH)/thirdparty/lib/android/libjsoncpp.a
+LOCAL_WHOLE_STATIC_LIBRARIES := jsoncpp
+
+#LOCAL_LDLIBS += $(LOCAL_PATH)/thirdparty/lib/android/libjsoncpp.a
+
 LOCAL_LDLIBS += $(LOCAL_PATH)/thirdparty/lib/android/libcurl.a
 
+LOCAL_LDLIBS += -L$(SYSROOT)/usr/lib -llog -lz
 
 
-include $(BUILD_EXECUTABLE)
-#include $(BUILD_STATIC_LIBRARY)
+
+#include $(BUILD_EXECUTABLE)
+include $(BUILD_SHARED_LIBRARY)
--- build/android/jni/Application.mk
+++ build/android/jni/Application.mk
@@ -1,7 +1,7 @@
 APP_PROJECT_PATH := $(call my-dir)/
 libjingle-root-dir:=$(APP_PROJECT_PATH)
 APP_BUILD_SCRIPT:=$(call my-dir)/Android.mk
-#APP_MODULES := curl libcurl
+APP_MODULES := libhttp-json
 APP_STL := gnustl_static
 APP_ABI := armeabi-v7a 
 #APP_ABI := armeabi
